67bea90d28510b2313b9a003259d064540bd81b0
Andrew Murray <radarhere@users.noreply.github.com>
2021-03-26 22:07:34 +1100

    Updated libavif to 0.9.0

--- winbuild/build_prepare.py
+++ winbuild/build_prepare.py
@@ -318,9 +318,9 @@ deps = {
         "bins": [r"libraqm.dll"],
     },
     "libavif": {
-        "url": "https://github.com/AOMediaCodec/libavif/archive/v0.8.4.zip",
-        "filename": "libavif-0.8.4.zip",
-        "dir": "libavif-0.8.4",
+        "url": "https://github.com/AOMediaCodec/libavif/archive/v0.9.0.zip",
+        "filename": "libavif-0.9.0.zip",
+        "dir": "libavif-0.9.0",
         "build": [
             cmd_cd("ext"),
             cmd_rmdir("aom"),
